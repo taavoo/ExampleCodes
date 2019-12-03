b4e97333958929568ac774fbf6f5062015d0de88
taavoo <GAONeil@student.fullsail.edu>
2019-12-02 22:03:46 -0500

    Create PlayerController.cs
    Uploaded master

---
+++ PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity -= transform.right * speed;
+        }
+    }
+}
