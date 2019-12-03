c5157ca0ff05356016ad099f357f31ca1c078122
taavoo <GAONeil@student.fullsail.edu>
2019-12-02 22:12:02 -0500

    Create Jump.cs
    added Jump to master

---
+++ Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float jumpHeight = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, 1, 0) * jumpHeight;
+        }
+    }
+}
